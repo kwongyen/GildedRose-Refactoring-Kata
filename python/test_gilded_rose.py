007ab7f8287b0d90e87bb4020f6c7497cb390990
Kwongyen Mok <kmok@beterdichtbij.nl>
2023-04-06 14:49:15 +0200

    first design

--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -6,10 +6,11 @@ from gilded_rose import Item, GildedRose
 
 class GildedRoseTest(unittest.TestCase):
     def test_foo(self):
-        items = [Item("foo", 0, 0)]
+        items = [Item("Aged Brie", 2, 0)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        self.assertEquals("fixme", items[0].name)
+        self.assertEquals("Aged Brie", items[0].name)
+        self.assertEquals(1, items[0].quality)
 
         
 if __name__ == '__main__':
